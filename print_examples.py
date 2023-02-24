c6bee6a1bd450eb10415c4e735fb0f29d0c14d0b
Sky-Wired <118227143+Sky-Wired@users.noreply.github.com>
2023-02-24 09:30:55 -0600

    Create print_examples.py

---
+++ print_examples.py
@@ -0,0 +1,9 @@
+a_random_integer = 8
+some_random_string = "Hi I am a string!"
+
+print("Hello World")
+print("This is the print command! It prints text and variables to the terminal!")
+print("Below is an example: ")
+print("Here is an example integer: ", a_random_integer, " and here is an example string: ", some_random_string)
+print("You can change these values that are in the code to be whatever you want. You could make a_random_integer = 1290921390 if you wanted. Same for the string!")
+print("Python is pretty cool!")
